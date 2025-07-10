72fee717c8c29d075938bcfd0f8970a40fb1cf90
Monish Nallagondalla <nsmonish@gmail.com>
2025-07-10 23:19:30 +0530

    update config

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -1,3 +1,19 @@
-import streamlit as st 
-import os 
-from datetime import date 
+import streamlit as st
+import os
+from datetime import date
+
+from langchain_core.messages import AIMessage,HumanMessage
+from src.langgraphagenticai.ui.uiconfigfile import Config
+
+
+class LoadStreamlitUI:
+
+    def __init__(self):
+        self.config = Config()
+        self.user_controls = {}
+
+    def initialize_session(self):
+        return {
+            "current_step":"requirements",
+            
+        }
